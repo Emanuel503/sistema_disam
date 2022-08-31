41d2c3ebdc0c983b7238eee57d0f6c087d11b39d
Emanuel Molina <emanueljosemolina@hotmail.es>
2022-08-31 10:55:00 -0600

    Vista
    Vista para el servidor en mantenimiento

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="text-center mt-5">Sistema en matenimiento</h1>
+    <p class="text-center">Pronto estara nuevamente en funcionamiento</p>
+@endsection
